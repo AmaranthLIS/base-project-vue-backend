0caf9a5a2ae576acb11f75927fe6965ecf0cc86a
lis <foxigs@gmail.com>
2020-12-19 02:45:25 +0400

    update app-config; create list of error

--- src/main/kotlin/ge/lis/cubiq/exception/Err.kt
+++ src/main/kotlin/ge/lis/cubiq/exception/Err.kt
@@ -4,5 +4,10 @@ package ge.lis.cubiq.exception
  * Created by
  * @Author: lis, Luganskiy Igor, foxigs@gmail.com
  * Date: 18.12.2020  * Time: 17:10
- */class Err {
+ */
+object Err {
+  const val INCORRECT_USERNAME: String = "Incorrect username";
+  const val INCORRECT_PASSWORD: String = "Incorrect password";
+  const val USER_NOT_FOUND: String = "Login or Password incorrect";
+  const val USER_WRONG_PASSWORD: String = "Login or Password incorrect";
 }
