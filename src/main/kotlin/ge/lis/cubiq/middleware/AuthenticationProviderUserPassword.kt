69846839a667eb18d2ca8a4296cbaf98a2b05104
lis <foxigs@gmail.com>
2020-12-18 13:56:42 +0400

    prepare workable AuthenticationProviderUserPassword with using DB and TaskExecutors.IO

--- src/main/kotlin/ge/lis/cubiq/middleware/AuthenticationProviderUserPassword.kt
+++ src/main/kotlin/ge/lis/cubiq/middleware/AuthenticationProviderUserPassword.kt
@@ -1,12 +1,19 @@
 package ge.lis.cubiq.middleware
 
+import ge.lis.cubiq.service.PasswordEncoderService
 import io.micronaut.http.HttpRequest
+import io.micronaut.scheduling.TaskExecutors
 import io.micronaut.security.authentication.*
 import io.reactivex.BackpressureStrategy
 import io.reactivex.Flowable
 import io.reactivex.FlowableEmitter
-import javax.inject.Singleton
+import io.reactivex.Scheduler
+import io.reactivex.schedulers.Schedulers
 import org.reactivestreams.Publisher
+import java.util.concurrent.ExecutorService
+import javax.inject.Inject
+import javax.inject.Named
+import javax.inject.Singleton
 
 /**
  * Created by
@@ -15,15 +22,32 @@ import org.reactivestreams.Publisher
  */
 @Singleton
 class AuthenticationProviderUserPassword : AuthenticationProvider {
+
+//  @Inject
+//  lateinit var userRepository: UserRepository
+  @Inject
+  lateinit var passwordEncoder: PasswordEncoderService
+
+  @Inject
+  @Named(TaskExecutors.IO)
+  lateinit var executorService: ExecutorService
+
+
+  private val scheduler: Scheduler by lazy {
+    Schedulers.from(executorService)
+  }
+
   override fun authenticate(httpRequest: HttpRequest<*>?, authenticationRequest: AuthenticationRequest<*, *>): Publisher<AuthenticationResponse> {
     return Flowable.create({ emitter: FlowableEmitter<AuthenticationResponse> ->
-      if (authenticationRequest.identity == "sherlock" && authenticationRequest.secret == "password") {//todo use DB
-        val userDetails = UserDetails(authenticationRequest.identity as String, ArrayList())
+//      UserState user = fetchUserState(authenticationRequest) //todo go to DB
+      if (authenticationRequest.identity == "sherlock" && authenticationRequest.secret == "password") {
+        val userDetails = UserDetails(authenticationRequest.identity as String, listOf("ROLE_ADMIN"), mapOf())//ArrayList()
         emitter.onNext(userDetails)
         emitter.onComplete()
       } else {
         emitter.onError(AuthenticationException(AuthenticationFailed()))
       }
     }, BackpressureStrategy.ERROR)
+        .subscribeOn(scheduler)
   }
 }
