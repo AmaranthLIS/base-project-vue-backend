03f1e04641ded9e4a6ee4f7e958b561a30c916a3
lis <foxigs@gmail.com>
2020-12-25 23:55:00 +0400

    Turn on PostgresPlugin

--- src/main/kotlin/ge/lis/cubiq/Application.kt
+++ src/main/kotlin/ge/lis/cubiq/Application.kt
@@ -1,14 +1,15 @@
 package ge.lis.cubiq
 
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize
 import io.micronaut.context.event.StartupEvent
 import io.micronaut.runtime.Micronaut.*
 import io.micronaut.runtime.event.annotation.EventListener
 import mu.KotlinLogging
 import org.jdbi.v3.core.Jdbi
 import org.jdbi.v3.core.kotlin.KotlinPlugin
-import org.jdbi.v3.jackson2.Jackson2Plugin
 import org.jdbi.v3.sqlobject.SqlObjectPlugin
 import org.jdbi.v3.sqlobject.kotlin.KotlinSqlObjectPlugin
+import org.jdbi.v3.postgres.PostgresPlugin
 import javax.inject.Inject
 import javax.inject.Singleton
 
@@ -33,10 +34,16 @@ class SampleEventListener {
     jdbi = jdbi.installPlugin(SqlObjectPlugin())
         .installPlugin(KotlinPlugin())
         .installPlugin(KotlinSqlObjectPlugin())
-        .installPlugin(Jackson2Plugin())
-//    jdbi.getConfig(Jackson2Config.class).setMapper(myObjectMapper)
-    logger.info { "StartupEvent - ${event.toString().substring(range = 27..38)}" }
+//        .installPlugin(Jackson2Plugin())
+        .installPlugin(PostgresPlugin())
+
+    logger.info { "StartupEvent - ${event.toString().substring(range = 27..38)}" } //        .getConfig(Jackson2Config.class).setMapper(Lang)
   }
 }
 
+
 typealias Tag = Pair<String, Any?>
+
+@JsonDeserialize
+data class Lang(var en: String? = null, var ru: String? = null) {}
+//@RegisterMapper divided into @RegisterRowMapper and @RegisterColumnMapper.
